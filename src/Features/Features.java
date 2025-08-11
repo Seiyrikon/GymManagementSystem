9807ad8735f0f053bb9f99b15319e9c16b0faddf
Seiryikon <rickygalpo7777@gmail.com>
2025-08-11 13:08:30 +0800

    Coding 2025/08/11 - Created custom exception for invalid choices

--- src/Features/Features.java
+++ src/Features/Features.java
@@ -1,12 +1,13 @@
 package Features;
 
+import Exception.InvalidChoiceException;
 import Subscription.Monthly;
 import Subscription.Subscription;
 import Subscription.Weekly;
 import Subscription.Yearly;
 
 public class Features {
-    public void registerMember(int choice, String id ,String uniqueIdentifier, String memberName, String dateOfAvailment, String membershipExpirationDate, String membershipType, String membershipStatus) {
+    public void registerMember(int choice, String id ,String uniqueIdentifier, String memberName, String dateOfAvailment, String membershipExpirationDate, String membershipType, String membershipStatus) throws InvalidChoiceException{
         switch (choice) {
             case 1:
                 Subscription yearly = new Yearly(id, uniqueIdentifier, memberName, dateOfAvailment, membershipExpirationDate, membershipType, membershipStatus);
@@ -20,7 +21,7 @@ public class Features {
                 Subscription weekly = new Weekly(id, uniqueIdentifier, memberName, dateOfAvailment, membershipExpirationDate, membershipType, membershipStatus);
                 System.out.println(weekly);
             default:
-                break;
+                throw new InvalidChoiceException("Registration only accepts choices 1, 2, and 3.");
         }
     }
 }
