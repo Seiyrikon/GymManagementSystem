00a57c6c9c72e1550f5d488d83a53185e8b33168
Seiryikon <rickygalpo7777@gmail.com>
2025-08-11 12:43:50 +0800

    Coding 2025/08/11 - Completed registerMember method

--- src/Features/Features.java
+++ src/Features/Features.java
@@ -1,6 +1,8 @@
 package Features;
 
+import Subscription.Monthly;
 import Subscription.Subscription;
+import Subscription.Weekly;
 import Subscription.Yearly;
 
 public class Features {
@@ -10,7 +12,13 @@ public class Features {
                 Subscription yearly = new Yearly(id, uniqueIdentifier, memberName, dateOfAvailment, membershipExpirationDate, membershipType, membershipStatus);
                 System.out.println(yearly);
                 break;
-
+            case 2: 
+                Subscription monthly = new Monthly(id, uniqueIdentifier, memberName, dateOfAvailment, membershipExpirationDate, membershipType, membershipStatus);
+                System.out.println(monthly);
+                break; 
+            case 3:
+                Subscription weekly = new Weekly(id, uniqueIdentifier, memberName, dateOfAvailment, membershipExpirationDate, membershipType, membershipStatus);
+                System.out.println(weekly);
             default:
                 break;
         }
